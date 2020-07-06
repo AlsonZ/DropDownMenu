ad52b7f1d2dfddeb74a839301bb218506078e3a3
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-07-06 20:05:02 +1000

    Updates

---
+++ precache-manifest.fe6aef18a78473a7f0e4d090a81f6f7c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c277648fc8773fb60f5f7384871879",
+    "url": "/DropDownMenu/index.html"
+  },
+  {
+    "revision": "7b5df66a8b5c2d62bbbd",
+    "url": "/DropDownMenu/static/css/main.0cd42d81.chunk.css"
+  },
+  {
+    "revision": "5b2226844c22f26433f3",
+    "url": "/DropDownMenu/static/js/2.c9aff05e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DropDownMenu/static/js/2.c9aff05e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b5df66a8b5c2d62bbbd",
+    "url": "/DropDownMenu/static/js/main.eb0db9e5.chunk.js"
+  },
+  {
+    "revision": "f2c1cb2c4b1bcc529c9e",
+    "url": "/DropDownMenu/static/js/runtime-main.bcacd997.js"
+  },
+  {
+    "revision": "6a28c4e91378519db2f2bc60370b0952",
+    "url": "/DropDownMenu/static/media/arrow.6a28c4e9.svg"
+  },
+  {
+    "revision": "2fe3b07e010ca2b5168ab2ac7c00fb8d",
+    "url": "/DropDownMenu/static/media/bell.2fe3b07e.svg"
+  },
+  {
+    "revision": "3660abd00cc9808e49523ee7f0135f49",
+    "url": "/DropDownMenu/static/media/caret.3660abd0.svg"
+  },
+  {
+    "revision": "f3b994200c19bfed6ce9a36e96e34674",
+    "url": "/DropDownMenu/static/media/chevron.f3b99420.svg"
+  },
+  {
+    "revision": "d76cd5fc4f09a1413f48199c19241127",
+    "url": "/DropDownMenu/static/media/cog.d76cd5fc.svg"
+  },
+  {
+    "revision": "6cf1ee7453ee7f8978627d96f61a8ad5",
+    "url": "/DropDownMenu/static/media/exit.6cf1ee74.svg"
+  },
+  {
+    "revision": "4a5d4bae360d44d0e1a1fe786bde6bf6",
+    "url": "/DropDownMenu/static/media/moon.4a5d4bae.svg"
+  },
+  {
+    "revision": "3575e29bf0067a33d375b9390759e98a",
+    "url": "/DropDownMenu/static/media/star.3575e29b.svg"
+  },
+  {
+    "revision": "c4a950540e97672c270be6bca894a6d8",
+    "url": "/DropDownMenu/static/media/sun.c4a95054.svg"
+  },
+  {
+    "revision": "d79bd7b63e77ee7482d8875d6f08ba40",
+    "url": "/DropDownMenu/static/media/user.d79bd7b6.svg"
+  }
+]);
